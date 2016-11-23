0f7b211e17a25615acb42cd71553bf643a651e86
rambou <nickos.bou@gmail.com>
2016-11-23 19:11:05 +0200

    Add project

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($) {
+    $(function() {
+        w3IncludeHTML();
+        $('.button-collapse').sideNav();
+        $('.dropdown-button').dropdown();
+        $('.parallax').parallax();
+        $('.collapsible').collapsible();
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
