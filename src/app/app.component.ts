c755bb8c35ffb53221751a1c91e9f9ed0d60bcc7
Nikolaos Bousios <nikolaos.bousios@sword-group.com>
2017-12-17 13:08:30 +0200

    Upgrade to ng5 * Minor fixes additions made also

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    //$window.pleaseWait.finish();
+    //console.log(window.document.body.plea.getElementById('loading_screen'))
+  }
+
 }
